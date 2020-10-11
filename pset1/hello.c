207a50f6aae17c866559ffa21228ad960869d894
Pawan Kishor Patil <63651567+PawanPatil19@users.noreply.github.com>
2020-10-11 22:17:38 +0700

    Update hello.c

--- pset1/hello.c
+++ pset1/hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 
 int main(void)
 {
-     printf("hello,world \n");
+     string name = get_string("What is your name?\n");
+     printf("hello, %s\n", name);
 }
